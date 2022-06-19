27bd26db2e88eafe525ad3af872545160758843e
pirateofspace <94983397+pirateofspace@users.noreply.github.com>
2022-06-19 17:35:12 +0200

    control LEDs to indicate power output

--- main.ts
+++ main.ts
@@ -1,5 +1,94 @@
-let pitch = 0
-let power = 0
+function controlLeds () {
+    if (power > 0 && power <= 10) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (power > 10 && power < 25) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . # . . .
+            # # . . .
+            `)
+    } else if (power > 25 && power <= 50) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . # # . .
+            # # # . .
+            `)
+    } else if (power > 50 && power <= 75) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . # # .
+            . # # # .
+            # # # # .
+            `)
+    } else if (power > 75 && power <= 100) {
+        basic.showLeds(`
+            . . . . #
+            . . . # #
+            . . # # #
+            . # # # #
+            # # # # #
+            `)
+    } else if (power < 0 && power >= -10) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (power < -10 && power >= -25) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . # .
+            . . . # #
+            `)
+    } else if (power < -25 && power >= -50) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . # # .
+            . . # # #
+            `)
+    } else if (power < -50 && power >= -75) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . # # . .
+            . # # # .
+            . # # # #
+            `)
+    } else if (power < -75 && power >= -100) {
+        basic.showLeds(`
+            # . . . .
+            # # . . .
+            # # # . .
+            # # # # .
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
+}
 function compute_power_value () {
     pitch = input.rotation(Rotation.Roll)
     power = pitch / 90
@@ -11,8 +100,10 @@ function compute_power_value () {
     }
     basic.showString("" + (power))
 }
+let pitch = 0
+let power = 0
+radio.setGroup(138)
 basic.forever(function () {
     compute_power_value()
-    radio.setGroup(138)
     radio.sendNumber(power)
 })
